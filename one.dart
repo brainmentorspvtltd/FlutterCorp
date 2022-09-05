18b83420db7529f4de3f10aae4308797dc7474fd
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-09-05 22:33:07 +0530

    Add files via upload

--- one.dart
+++ one.dart
@@ -1,30 +1,44 @@
-show(var x, var y) {
-  print(x.runtimeType);
-  print(y.runtimeType);
-  if (x.runtimeType == int && y.runtimeType == String) {
-    print("Inside If ...");
-    return '$x $y';
-  } else {
-    return x + y;
+// Imperative Programming
+List<int> list = [9000, 4000, 5000, 7000, 2000, 1000];
+int compute() {
+  int sum = 0;
+  for (int i = 0; i < list.length; i++) {
+    if (list[i] >= 5000) {
+      sum = sum + list[i];
+    }
   }
+  return sum;
 }
 
+class Emp {
+  late int id;
+  late String name;
+  late double salary;
+  Emp.takeInput(this.id, this.name, this.salary);
+
+  @override
+  String toString() => 'Emp(id: $id, name: $name, salary: $salary)';
+}
+
+// Declarative Programming
 void main() {
-  print(show(10, 20));
-  print(show(10, "Ten"));
-  print("Hello Dart");
-  int a;
-  String b;
-  var g;
-  g = 100;
-  g = "Amit";
-  var z = 100;
-  //z = "Amit";
-  dynamic q = 1000;
-  q = true;
-  q = [];
-  int age = 21;
-  print("Age is ${age + 1}");
-  print("Age is $age");
-  print("Age is " + age.toString());
+  List<Emp> empList = [
+    Emp.takeInput(10, "Ram", 29000),
+    Emp.takeInput(11, "Ramesh", 19000),
+    Emp.takeInput(1, "Raju", 39000),
+    Emp.takeInput(2, "Shyam", 9000)
+  ];
+  Emp empObj = empList.firstWhere((Emp emp) => emp.name == 'Raju');
+  print(empObj);
+  list.sort((int first, int second) => second - first);
+  print(list);
+  List<int> r2 = list.where((int element) => element >= 5000).toList();
+  print(r2);
+  int r = list.fold(0, (int prev, int current) {
+    if (current >= 5000) {
+      prev = prev + current;
+    }
+    return prev;
+  });
+  print(r);
 }
